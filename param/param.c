e7928b33ad2535345565772054f5e410e91b0845
zhangliuying <dear531@21cn.com>
2015-04-13 16:46:31 +0800

    usage of param for pointer char array

---
+++ param/param.c
@@ -0,0 +1,32 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/param.h>
+
+static int num = 0;
+static char *p[10] = {NULL};
+
+module_param_array(p, charp, &num, S_IRUGO);
+
+static int __init param_init(void)
+{
+	int i;
+	for (i = 0; num > i; i++)
+		printk(KERN_ALERT "p[%d]:%s\n",
+				i, p[i]);
+	printk(KERN_ALERT "num :%d\n", num);
+	return 0;
+}
+
+static void __exit param_exit(void)
+{
+	printk(KERN_ALERT "param_exit\n");
+}
+
+module_init(param_init);
+module_exit(param_exit);
+
+MODULE_LICENSE("gpl");
+MODULE_AUTHOR("zhangly");
+MODULE_VERSION("2.6.30");
+MODULE_ALIAS();
+MODULE_DESCRIPTION();
