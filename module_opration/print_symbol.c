1304ec4ca93e5f8a1b53af2301bb59d9db906984
zhangly <dear531@21cn.com>
2015-04-29 17:16:13 +0800

    append usage of print_symbol

--- module_opration/print_symbol.c
+++ module_opration/print_symbol.c
@@ -10,6 +10,7 @@ static int a_symbol(void)
 
 EXPORT_SYMBOL(a_symbol);
 
+unsigned long	address;
 static int __init print_symbol_init(void)
 {
 	/*
@@ -18,7 +19,6 @@ static int __init print_symbol_init(void)
 	const char	*fmt = "this is first part %s\n";
 	struct module 	*fmodule;
 	char		*name = NULL;
-	unsigned long	address;
 	printk(KERN_INFO "print_symbol_init\n");
 	address = (unsigned long)__builtin_return_address(0);
 	__print_symbol(fmt, address);
@@ -31,8 +31,19 @@ static int __init print_symbol_init(void)
 		printk(KERN_INFO "module name %s\n",
 				fmodule->name);
 		address = (unsigned long)fmodule->module_core;
+#if 0
+	/* Here are the sizes of the init and core sections */
+	unsigned int init_size, core_size;
+
+	/* The size of the executable code in each section.  */
+	unsigned int init_text_size, core_text_size;
+#endif
 		fmt = "this is second part :%s\n";
 		__print_symbol(fmt, address);
+		printk(KERN_INFO "init size :%x, core_size :%x\n",
+				fmodule->init_size, fmodule->core_size);
+		printk(KERN_INFO "text size :%x, text core_size :%x\n",
+				fmodule->init_text_size, fmodule->core_text_size);
 	}
 	address = (unsigned long)a_symbol + 5;
 	fmt = "this is thrid part :%s\n";
@@ -42,7 +53,10 @@ static int __init print_symbol_init(void)
 
 static void __exit print_symbol_exit(void)
 {
+	const char	*fmt = "this is exit part %s\n";
 	printk(KERN_INFO "print_symbol_exit\n");
+	address = (unsigned long)__builtin_return_address(0);
+	__print_symbol(fmt, address);
 }
 
 module_init(print_symbol_init);
