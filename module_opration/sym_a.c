063586dfb327c8cc8423b9413f0cedd9a925ca12
zhangly <dear531@21cn.com>
2015-04-29 18:23:08 +0800

    append contain of function sym_a

--- module_opration/sym_a.c
+++ module_opration/sym_a.c
@@ -1,9 +1,25 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/kallsyms.h>
 
 static int sym_a(void)
 {
+	char buff[KSYM_SYMBOL_LEN] = {0};
+	unsigned long address;
+	const struct kernel_symbol *sym;
+	printk(KERN_INFO "sym_a\n");
+	address = (unsigned long)__builtin_return_address(0);
+	printk(KERN_INFO "builtin_return_address :%lx\n",
+			address);
+	address = (unsigned long)sym_a;
+	printk(KERN_INFO "sym_a :%lx\n",
+			address);
+	sym = find_symbol("sym_a", NULL, NULL, true, true);
+	if (NULL != sym) {
+		printk(KERN_INFO "sym->name :%s, sym->value :%lx\n",
+				sym->name, sym->value);
+	}
 	return 0;
 }
 EXPORT_SYMBOL(sym_a);
