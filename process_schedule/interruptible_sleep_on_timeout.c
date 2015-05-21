7ab76afeb51c317110b3d86c4ae3f7bdc0dbf6ad
zhangly <dear531@21cn.com>
2015-05-21 13:11:55 +0800

    usage of interruptible_sleep_on_timeout

---
+++ process_schedule/interruptible_sleep_on_timeout.c
@@ -0,0 +1,63 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+
+static int myfunc(void *arg)
+{
+	printk(KERN_INFO "child pid : %d parent pid :%d in child\n",
+			current->pid, current->real_parent->pid);
+	printk(KERN_INFO "parent state :%ld befor wake_up_process\n",
+			current->real_parent->state);
+#if 0
+#if 0
+int wake_up_process(struct task_struct *p)
+#endif
+	wake_up_process(current->real_parent);
+	printk(KERN_INFO "parent state :%ld after wake_up_process\n",
+			current->real_parent->state);
+#endif
+	return 0;
+}
+
+static int __init interruptible_sleep_on_timeout_init(void)
+{
+	pid_t pid;
+	wait_queue_head_t head;
+	wait_queue_t data;
+	long remaintime;
+	printk(KERN_INFO "interruptible_sleep_on_timeout_init\n");
+#if 0
+pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags)
+#endif
+	pid = kernel_thread(myfunc, NULL, CLONE_KERNEL);
+#if 0
+void init_waitqueue_head(wait_queue_head_t *q)
+#endif
+	init_waitqueue_head(&head);
+#if 0
+static inline void init_waitqueue_entry(wait_queue_t *q, struct task_struct *p)
+#endif
+	init_waitqueue_entry(&data, current);
+#if 0
+void add_wait_queue(wait_queue_head_t *q, wait_queue_t *wait)
+#endif
+	add_wait_queue(&head, &data);
+#if 0
+interruptible_sleep_on_timeout(wait_queue_head_t *q, long timeout)
+#endif
+	remaintime = interruptible_sleep_on_timeout(&head, 100);
+	printk(KERN_INFO "remaintime :%ld\n",
+			remaintime);
+	return 0;
+}
+
+static void __exit interruptible_sleep_on_timeout_exit(void)
+{
+	printk(KERN_INFO "interruptible_sleep_on_timeout_exit\n");
+}
+
+module_init(interruptible_sleep_on_timeout_init);
+module_exit(interruptible_sleep_on_timeout_exit);
+
+MODULE_LICENSE("GPL");
