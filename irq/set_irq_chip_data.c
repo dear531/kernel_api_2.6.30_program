4a6cd67a985b0f43622dc331efe42c4da72cc3b5
zhangly <dear531@21cn.com>
2015-06-01 09:54:08 +0800

    usage of set_irq_chip_data

---
+++ irq/set_irq_chip_data.c
@@ -0,0 +1,54 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+
+unsigned int irq = 5;
+#if 0
+typedef irqreturn_t (*irq_handler_t)(int, void *);
+#endif
+irqreturn_t irq_handler(int irq, void *dev_id)
+{
+	printk(KERN_INFO "%s\n", __func__);
+	return IRQ_NONE;
+}
+
+irqreturn_t irq_thread(int irq, void *dev_id)
+{
+	printk(KERN_INFO "%s\n", __func__);
+	return IRQ_NONE;
+}
+
+static int __init set_irq_chip_data_init(void)
+{
+	int ret;
+	int data;
+	printk(KERN_INFO "set_irq_chip_data_init\n");
+#if 0
+extern int __must_check
+request_threaded_irq(unsigned int irq, irq_handler_t handler,
+		     irq_handler_t thread_fn,
+		     unsigned long flags, const char *name, void *dev);
+#endif
+	ret = request_threaded_irq(irq, irq_handler, irq_thread,
+			IRQF_DISABLED, "A_NEW_DEVICE", NULL);
+	printk(KERN_INFO "request_threaded_irq result :%d\n", ret);
+#if 0
+int set_irq_chip_data(unsigned int irq, void *data)
+#endif
+	ret = set_irq_chip_data(irq, &data);
+	printk(KERN_INFO "set_irq_chip_data result :%d\n", ret);
+	return 0;
+}
+
+static void __exit set_irq_chip_data_exit(void)
+{
+	printk(KERN_INFO "set_irq_chip_data_exit\n");
+	free_irq(irq, NULL);
+}
+
+module_init(set_irq_chip_data_init);
+module_exit(set_irq_chip_data_exit);
+
+MODULE_LICENSE("GPL");
