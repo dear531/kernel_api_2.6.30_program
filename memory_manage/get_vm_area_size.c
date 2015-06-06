ee51cdf7d143a2fff43dd4019aa6d64ff18fdf70
zhangly <dear531@21cn.com>
2015-06-06 21:26:42 +0800

    usage of get_vm_area_size

---
+++ memory_manage/get_vm_area_size.c
@@ -0,0 +1,42 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/mm.h>
+#include <linux/vmalloc.h>
+
+struct vm_struct *vm = NULL;
+
+static int __init get_vm_area_size_init(void)
+{
+	size_t size = 8192, actual_size;
+	int ret = 0;
+	printk(KERN_INFO "%s\n", __func__);
+#if 0
+struct vm_struct *alloc_vm_area(size_t size)
+#endif
+	vm = alloc_vm_area(size);
+	if (NULL == vm) {
+		ret = -ENOMEM;
+		goto finish;
+	}
+#if 0
+static inline size_t get_vm_area_size(const struct vm_struct *area)
+#endif
+	actual_size = get_vm_area_size(vm);
+	printk(KERN_INFO "actual_size :%d\n", actual_size);
+finish:
+	return ret;
+}
+
+static void __exit get_vm_area_size_exit(void)
+{
+	printk(KERN_INFO "%s\n", __func__);
+#if 0
+void free_vm_area(struct vm_struct *area)
+#endif
+	free_vm_area(vm);
+}
+
+module_init(get_vm_area_size_init);
+module_exit(get_vm_area_size_exit);
+
+MODULE_LICENSE("GPL");
